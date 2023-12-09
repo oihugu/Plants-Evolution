5aa61c06cc36bd3cfc620abfeff7764ee4403555
oihugu <oihugu@gmail.com>
2023-12-09 18:27:56 -0300

    Rule And LSystems Unit Tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "../src/LSystem/include/LSystem.hpp"
 #include "../src/LSystem/include/Rule.hpp"
+
 #include <gtest/gtest.h>
+
 
 
 // Demonstrate some basic assertions.
@@ -10,3 +16,51 @@ TEST(HelloTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
+
+//////////////////////////////////////////
+// Rule Tests
+//////////////////////////////////////////
+
+TEST(Rule, Constructor) {
+    Rule rule('A', "AB");
+    EXPECT_EQ(rule.getInitChar(), 'A');
+    EXPECT_EQ(rule.getReplacement(), "AB");
+}
+
+TEST(Rule, Setters) {
+    Rule rule('A', "AB");
+    rule.setInitChar('B');
+    rule.setReplacement("BA");
+    EXPECT_EQ(rule.getInitChar(), 'B');
+    EXPECT_EQ(rule.getReplacement(), "BA");
+}
+
+TEST(Rules, Replace) {
+    Rule rule('A', "AB");
+    string str = "A";
+    rule.replace(str);
+    EXPECT_EQ(str, "AB");
+}
+
+//////////////////////////////////////////
+// LSystem Tests
+//////////////////////////////////////////
+
+TEST(LSystem, Constructor) {
+    LSystem lsystem(vector<char>{'A','B'},
+            "A",
+            vector<Rule>{Rule('A',"AB"), Rule('B',"BA")});
+
+    EXPECT_EQ(lsystem.getState(), "A");
+}
+
+TEST(LSystem, ApplyRules) {
+    LSystem lsystem(vector<char>{'A','B'},
+            "A",
+            vector<Rule>{Rule('A',"AB"), Rule('B',"BA")});
+
+    lsystem.applyRules();
+
+    EXPECT_EQ(lsystem.getState(), "ABA");
+}
+
